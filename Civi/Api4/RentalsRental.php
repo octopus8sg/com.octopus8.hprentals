c33a7362321c5830acf1c6df980678e0283b1966
tixuz <hindolmirzo@gmail.com>
2023-03-22 02:32:52 +0300

    started rental-2

---
+++ Civi/Api4/RentalsRental.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * RentalsRental entity.
+ *
+ * Provided by the HP Rentals extension.
+ *
+ * @package Civi\Api4
+ */
+class RentalsRental extends Generic\DAOEntity {
+
+}
